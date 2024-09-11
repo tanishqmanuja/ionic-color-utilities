4e31ed7e0659913df6621ff162bfd07e942b72a5
tanishqmanuja <tanishqmanuja@gmail.com>
2024-09-11 18:01:13 +0530

    🚑 fix: steps type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -29,13 +29,21 @@ export type ColorNames = (typeof COLOR_NAMES)[number];
 export type Steps =
   | 50
   | 100
+  | 150
   | 200
+  | 250
   | 300
+  | 350
   | 400
+  | 450
   | 500
+  | 550
   | 600
+  | 650
   | 700
+  | 750
   | 800
+  | 850
   | 900
   | 950;
 
